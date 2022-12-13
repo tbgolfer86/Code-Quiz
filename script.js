15be5be1bcf071bfc6f06b9b8aa8459ab6a8e059
tbgolfer86 <tbj86@outlook.com>
2022-12-13 16:42:52 -0500

    added IDs to buttons and changed css

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var submitButton = document.getElementById("submit");
 var goBackButton = document.getElementById("go-back");
 var clearHighScoresButton = document.getElementById("clear-high-scores");
 
-
+var score = 0;
+var questions = [];
 var timer = document.getElementById("timer");
 var finalScore = document.getElementById("final-score");
