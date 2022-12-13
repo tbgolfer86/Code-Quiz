a1b885b7cc42d601d2ef0116f1fad8338b14b934
tbgolfer86 <tbj86@outlook.com>
2022-12-13 17:24:08 -0500

    added comments to CSS and cleaned up HTML

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var viewHighScoresButton = document.getElementById("view-high-scores");
-var startButton = document.getElementById("start");
-var submitButton = document.getElementById("submit");
-var goBackButton = document.getElementById("go-back");
-var clearHighScoresButton = document.getElementById("clear-high-scores");
 
-var score = 0;
-var questions = [];
-var timer = document.getElementById("timer");
-var finalScore = document.getElementById("final-score");
+
+//Commonly used data types do not include:
+//strings, booleans, numbers, prompts
+
+//Arrays in Javascript can be used to store:
+//numbers, strings, booleans, all of the above
+
+//String values are enclosed in:
+// "", <>, (), #
+
+//The coding language that makes websites interactive is:
+//HTML, CSS, Javascript, French
