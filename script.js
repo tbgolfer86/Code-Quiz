9b5808d42b73e3447d7d715f60938624844d1768
tbgolfer86 <tbj86@outlook.com>
2022-12-13 18:06:36 -0500

    added Javascript

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 
+//Game Timer
+var timerElement = document.getElementById("timer");
+var secondsLeft = 60;
+function setTimer() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerElement.textContent = "Timer: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      return;
+    }
+
+  }, 1000);
+}
+
+//Start Game
+document.getElementById("start-button").addEventListener("click", function() {
+    setTimer();
+});
+
+
+
+
+
+
+
+
 
 //Commonly used data types do not include:
 //strings, booleans, numbers, prompts
