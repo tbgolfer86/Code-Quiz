31d6b1ddb3bedf836d7ec1293a6d2e016263b6d5
tbgolfer86 <tbj86@outlook.com>
2022-12-13 16:07:22 -0500

    organized and added to HTML and CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+var viewHighScoresButton = document.getElementById("view-high-scores");
+var startButton = document.getElementById("start");
+var submitButton = document.getElementById("submit");
+var goBackButton = document.getElementById("go-back");
+var clearHighScoresButton = document.getElementById("clear-high-scores");
+
+
+var timer = document.getElementById("timer");
+var finalScore = document.getElementById("final-score");
