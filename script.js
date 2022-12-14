14015c3d16318ff32c210c8365c10e7d5af8c6ca
tbgolfer86 <tbj86@outlook.com>
2022-12-14 15:16:10 -0500

    added functionality to buttons in .js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var quizData = [{
     choice2: "booleans",
     choice3: "numbers",
     choice4: "prompts",
-    answer: 3
+    answer: "prompts"
 },
 {   
     question: "Arrays in Javascript can be used to store: ",
@@ -12,7 +12,7 @@ var quizData = [{
     choice2: "strings",
     choice3: "objects",
     choice4: "all of the above",
-    answer: 3
+    answer: "all of the above"
 },
 {
     question: "String values are enclosed in: ",
@@ -20,7 +20,7 @@ var quizData = [{
     choice2: "''",
     choice3: "<>",
     choice4: "{}",
-    answer: 1
+    answer: "''"
 },
 {
     question: "The coding language that makes websites interactive is: ",
@@ -28,9 +28,8 @@ var quizData = [{
     choice2: "CSS",
     choice3: "Javascript",
     choice4: "none of the above",
-    answer: 2
-}];
-console.log(quizData);
+    answer: "Javascript"
+},0];
 
 var question = document.getElementById("question");
 var option1 = document.getElementById("choice1");
@@ -41,6 +40,12 @@ var option4 = document.getElementById("choice4");
 var timerElement = document.getElementById("timer");
 var timer;
 var timerCount = 60;
+var score = document.getElementById("score");
+
+//Display Score
+function getScore() {
+    score.textContent = "Your final score: " + timerCount;
+}
 
 //Game Timer function
 function startTimer() {
@@ -60,6 +65,7 @@ document.getElementById("view-high-scores").addEventListener("click", function()
     document.getElementById("quiz-intro-screen").style.display = "none";
     document.getElementById("questions-screen").style.display = "none";
     document.getElementById("quiz-finished-screen").style.display = "none";
+    console.log("clicked");
 });
 
 //Go back button
@@ -78,19 +84,93 @@ document.getElementById("start-button").addEventListener("click", function() {
     displayQuiz();
 });
 
-
-
+var numberChoice = 0;
 
 //Display quizData onto screen
 function displayQuiz() {
-    for (i = 0; i < quizData.length; i++) {
-        question.textContent = quizData[i].question;
-        option1.textContent = quizData[i].choice1;
-        option2.textContent = quizData[i].choice2;
-        option3.textContent = quizData[i].choice3;
-        option4.textContent = quizData[i].choice4;
-    } 
+        question.textContent = quizData[numberChoice].question;
+        option1.textContent = quizData[numberChoice].choice1;
+        option2.textContent = quizData[numberChoice].choice2;
+        option3.textContent = quizData[numberChoice].choice3;
+        option4.textContent = quizData[numberChoice].choice4;
 };
+
+//Display quiz finished screen
+function displayFinish () {
+    if (numberChoice > 3) {
+        document.getElementById("quiz-finished-screen").style.display = "flex";
+        document.getElementById("quiz-intro-screen").style.display = "none";
+        document.getElementById("high-scores-screen").style.display = "none";
+        document.getElementById("questions-screen").style.display = "none";
+        getScore();
+    }
+}
+
+//validate buttons
+btn1.addEventListener("click", function() {
+    if (btn1.clicked == true) {
+        numberChoice = numberChoice++;
+    } if (question.textContent === "Commonly used data types do not include: ") {
+        (timerCount = timerCount - 10);   
+    } if (question.textContent === "Arrays in Javascript can be used to store: ") {
+        (timerCount = timerCount - 10);
+    } if (question.textContent === "String values are enclosed in: ") {
+        (timerCount = timerCount - 10);
+    } if (question.textContent === "The coding language that makes websites interactive is: " ) {
+        (timerCount = timerCount - 10);
+    } 
+    displayQuiz();
+    displayFinish();
+    console.log("click")
+    console.log(numberChoice++)
+});
+        
+btn2.addEventListener("click", function() {
+    if (btn2.clicked == true) {
+        numberChoice = numberChoice++;
+    } if (question.textContent === "Commonly used data types do not include: ") {
+        (timerCount = timerCount - 10);
+    } if (question.textContent === "Arrays in Javascript can be used to store: ") {
+        (timerCount = timerCount - 10);
+    } if (question.textContent === "The coding language that makes websites interactive is: " ) {
+        (timerCount = timerCount - 10);
+    } 
+    displayQuiz();
+    displayFinish();
+    console.log("click")
+    console.log(numberChoice++)
+});
+
+btn3.addEventListener("click", function() {
+    if (btn3.clicked == true) {
+        numberChoice = numberChoice++;
+    } if (question.textContent === "Commonly used data types do not include: ") {
+        (timerCount = timerCount - 10);
+    } if (question.textContent === "Arrays in Javascript can be used to store: ") {
+        (timerCount = timerCount - 10);
+    } if (question.textContent === "String values are enclosed in: ") {
+        (timerCount = timerCount - 10);
+    } 
+    displayQuiz();
+    displayFinish();
+    console.log("click")
+    console.log(numberChoice++)
+});
+
+btn4.addEventListener("click", function() {
+    if (btn4.clicked == true) {
+        numberChoice = numberChoice++;
+    } if (question.textContent === "String values are enclosed in: ") {
+        (timerCount = timerCount - 10);
+    } if (question.textContent === "The coding language that makes websites interactive is: " ) {
+        (timerCount = timerCount - 10);
+    }
+    displayQuiz();
+    displayFinish();
+    console.log("click")
+    console.log(numberChoice++)
+});
+
 
 
 
