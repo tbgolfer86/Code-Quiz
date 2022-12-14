0fb13b4fbac81fa2c01b083bb74bf2ae637e75bd
tbgolfer86 <tbj86@outlook.com>
2022-12-13 22:12:01 -0500

    renamed some variables and ids

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
 var quizData = [{
     question: "Commonly used data types do not include: ",
-    choice0: "strings",
-    choice1: "booleans",
-    choice2: "numbers",
-    choice3: "prompts",
+    choice1: "strings",
+    choice2: "booleans",
+    choice3: "numbers",
+    choice4: "prompts",
     answer: 3
 },
 {   
     question: "Arrays in Javascript can be used to store: ",
-    choice0: "numbers",
-    choice1: "strings",
-    choice2: "objects",
-    choice3: "all of the above",
+    choice1: "numbers",
+    choice2: "strings",
+    choice3: "objects",
+    choice4: "all of the above",
     answer: 3
 },
 {
     question: "String values are enclosed in: ",
-    choice0: "&&",
-    choice1: "''",
-    choice2: "<>",
-    choice3: "{}",
+    choice1: "&&",
+    choice2: "''",
+    choice3: "<>",
+    choice4: "{}",
     answer: 1
 },
 {
     question: "The coding language that makes websites interactive is: ",
-    choice0: "HTML",
-    choice1: "CSS",
-    choice2: "Javascript",
-    choice3: "none of the above",
+    choice1: "HTML",
+    choice2: "CSS",
+    choice3: "Javascript",
+    choice4: "none of the above",
     answer: 2
 }];
 console.log(quizData);
 
-var questions = document.getElementById("question");
-var option0 = document.getElementById("choice0");
+var question = document.getElementById("question");
 var option1 = document.getElementById("choice1");
 var option2 = document.getElementById("choice2");
 var option3 = document.getElementById("choice3");
+var option4 = document.getElementById("choice4");
+
+
 
 //Display quizData onto screen
 function displayQuiz() {
     for (i = 0; i < quizData.length; i++) {
-        questions.textContent = quizData[i].question;
-        option0.textContent = quizData[i].choice0;
+        question.textContent = quizData[i].question;
         option1.textContent = quizData[i].choice1;
         option2.textContent = quizData[i].choice2;
         option3.textContent = quizData[i].choice3;
+        option4.textContent = quizData[i].choice4;
     } 
-}
+};
 
 //Game Timer
 var timerElement = document.getElementById("timer");
 var secondsLeft = 60;
-
 function setTimer() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
