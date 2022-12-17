5d5bb0de0b6da1679a1cdfa59a6464108795ba20
tbgolfer86 <tbj86@outlook.com>
2022-12-17 10:41:27 -0500

    added if statement to submit button

--- script.js
+++ script.js
@@ -41,6 +41,10 @@ var timerCount = 60;
 //Submit score and initials to scoreboard
 document.getElementById("submit").addEventListener("click", function(event) {
     event.preventDefault();
+    if (intials.value.length > 3) {
+        alert("Enter up to 3 letters");
+        return;
+    }
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
     var newScore = {
         score: timerCount,
